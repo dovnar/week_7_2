603043ea9fa3ffc002d588dbbf67c44a4a29d711
introkun <introkun@gmail.com>
2016-07-10 14:59:56 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// Do not use system function name for your functions
 void Sleep(BYTE time)
 {
 	Sleep((DWORD)time * 100);
@@ -16,14 +17,14 @@ int main()
 	char sign;
 	int line, speed, size;
 	cout << "выводим линию горезонтальную или вертикальную, необходимы некоторые данные \n"
-		"введите символ которым вы хотите е вывести = ";
+		"введите символ которым вы хотите ее вывести = ";
 	cin >> sign;
 	cout << "введите количество символов от 2 до 30 = ";
 	cin >> size;
-	cout << "выберите какую линию будем выводить \nгорезонтальную 1 \nвертикальную 2 \n= ";
-	cin >> line;
+	cout << "выберите какую линию будем выводить \nгоризонтальную 1 \nвертикальную 2 \n= ";
+	cin >> line; // нужен enum для хранения такого рода данных
 	cout << "выберете скорость ввываод линии \nмедлено 1 \nсредне 2 \nбыстро 3\n= ";
-	cin >> speed;
+	cin >> speed; // нужен enum для хранения такого рода данных
 	if (speed > 3 ||
 		speed < 1 ||
 		line < 1 ||
@@ -32,11 +33,12 @@ int main()
 		size>30)
 	{
 		cout << "ошибка при вводе данных!! проверьте правельность ввода, убедитесь что во время ввода у вас не нажат Shift";
+		return 1;
 	}
 	for (int i = 0; i < size; i++)
 	{
-		line == 1 ? cout << sign : cout << sign << endl;
-		speed == 1 ?  Sleep((BYTE)20) : speed == 2 ? Sleep((BYTE)10) : Sleep((BYTE)1);
+		cout << (line == 1 ? sign : sign + '\n');
+		speed == 1 ?  Sleep((BYTE)20) : speed == 2 ? Sleep((BYTE)10) : Sleep((BYTE)1); // лучше использовать switch
 	}
 	cout << endl;
 }
